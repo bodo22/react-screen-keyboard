e7881b884800ebc8aa29206d1961ece2ad079c1c
Wilhelm Herbrich <herbrich@sensape.com>
2018-09-14 20:20:13 +0200

    Add more responsive pressed feedback for user

--- src/KeyboardButton.js
+++ src/KeyboardButton.js
@@ -1,7 +1,12 @@
-import React, {PureComponent} from 'react';
+import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 
 export default class KeyboardButton extends PureComponent {
+
+	state = {
+		isPressed: false,
+	}
+
 	static propTypes = {
 		value: PropTypes.oneOfType([PropTypes.string.isRequired, PropTypes.node.isRequired]),
 		classes: PropTypes.string,
@@ -15,14 +20,29 @@ export default class KeyboardButton extends PureComponent {
 		isDisabled: false,
 	};
 
-	handleClick = () => this.props.onClick(this.props.value)
+	handleClick = () => {
+		this.setState({ isPressed: true });
+		this.props.onClick(this.props.value);
+	}
+
+	handleRelease = () => {
+		if (this.state.isPressed === true) {
+			this.setState({ isPressed: false });
+		}
+	}
 
 	render() {
+
+		const pressedCls = this.state.isPressed === true ? 'keyboard-button-pressed' : '';
+
 		return (
 			<button
 				type="button"
-				className={`keyboard-button ${this.props.classes}`}
+				className={`keyboard-button ${this.props.classes} ${pressedCls}`}
 				onPointerDown={this.props.isDisabled ? null : this.handleClick}
+				onPointerUp={this.handleRelease}
+				onPointerOut={this.handleRelease}
+				onPointerLeave={this.handleRelease}
 				autoFocus={this.props.autofocus}
 				disabled={this.props.isDisabled}
 			>
